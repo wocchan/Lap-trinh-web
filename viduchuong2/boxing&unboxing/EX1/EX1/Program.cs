0e0c604346a3b9c5ad9242d1403a993e56cbdd30
wocchan <144324224+wocchan@users.noreply.github.com>
2025-02-09 17:13:37 +0700

    Add files via upload

---
+++ viduchuong2/boxing&unboxing/EX1/EX1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 123;
+            // boxing copies the value of i into object o
+            object o = i;
+            // change the value of i
+            i = 456;
+            // the change in i doesn't addect the value stored in o
+            Console.WriteLine("The value-type value = {0}", i);
+            Console.WriteLine("the object-type value = {0}", o);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ viduchuong2/boxing&unboxing/EX2/EX2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 123; // a value type
+            object o = i; // boxing
+            int j = (int)o; //unboxing
+            Console.WriteLine("i = {0}, o = {1}, j = {2}", i, o, j);
+            Console.ReadLine();
+        }
+    }
+}
